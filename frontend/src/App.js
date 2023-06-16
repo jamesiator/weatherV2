24a3f3adc073f215ff65969304c5293f6e4fd8f6
James Belnap <james6bcdn@gmail.com>
2023-06-16 00:51:10 -0600

    Add base react files, WIP building home page

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import logo from './tornado.png';
+import Body from './components/Body';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className='App-logo' alt='logo'/>
+        <p>
+          <em>Weather</em> or not
+        </p>
+      </header>
+      <Body />
+      <footer className='App-footer'>
+        <p>
+          -Tornado clipart from <em>OpenClipart-Vectors</em> on <a href='https://pixabay.com' className='App-link'>Pixabay</a>-
+        </p>
+        <p>
+          -Weather data and icons from 
+          <a href='https://openweathermap.org' className='App-link'>openweathermap</a>-
+        </p>
+        <br/>
+        <p>
+          <em>a project by James Belnap</em>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { TextField } from '@mui/material';
+import './Body.css';
+
+function Body() {
+  return (
+    <div className='App-body'>
+      Hello
+    </div>
+  );
+}
+
+export default Body;
